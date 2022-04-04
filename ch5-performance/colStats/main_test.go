da849c86c526eac2121ca7ae87d26896e01aa5d9
Neil Stoker <neil.stoker@gmail.com>
2022-04-04 22:11:16 +0100

    initial column statistics app

---
+++ ch5-performance/colStats/main_test.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"os"
+	"testing"
+)
+
+func TestRun(t *testing.T) {
+	testCases := []struct {
+		name   string
+		col    int
+		op     string
+		exp    string
+		files  []string
+		expErr error
+	}{
+		{name: "RunAvg1file", col: 3, op: "avg", exp: "227.6\n",
+			files:  []string{"./testdata/example.csv"},
+			expErr: nil,
+		},
+		{name: "RunAvgMultiFiles", col: 3, op: "avg", exp: "233.84\n",
+			files:  []string{"./testdata/example.csv", "./testdata/example2.csv"},
+			expErr: nil},
+		{name: "RunFailRead", col: 2, op: "avg", exp: "",
+			files:  []string{"./testdata.example.csv", "./testdata/fakefile.csv"},
+			expErr: os.ErrNotExist},
+		{name: "RunFailNoFiles", col: 2, op: "avg", exp: "",
+			files: []string{}, expErr: ErrNoFiles},
+		{name: "RunFailOperation", col: 2, op: "invalid", exp: "",
+			files:  []string{"./testdata/example.csv"},
+			expErr: ErrInvalidOperation},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			var res bytes.Buffer
+			err := run(tc.files, tc.op, tc.col, &res)
+			if tc.expErr != nil {
+				if err == nil {
+					t.Errorf("%s - Expected error. Got nil instead", tc.name)
+				}
+
+				if !errors.Is(err, tc.expErr) {
+					t.Errorf("%s - Expected error %q, got %q instead", tc.name, tc.exp, err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Errorf("%s - Unexpected error %q", tc.name, err)
+			}
+
+			if res.String() != tc.exp {
+				t.Errorf("%s - Expected %q, got %q instead", tc.name, tc.exp, &res)
+			}
+		})
+	}
+}
