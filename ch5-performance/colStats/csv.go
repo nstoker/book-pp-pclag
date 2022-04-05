95d6b14813c7ac1ba9e7f1c5ad54c2aae6c9aa03
Neil Stoker <neil.stoker@gmail.com>
2022-04-05 21:17:06 +0100

    changes due to profiling

--- ch5-performance/colStats/csv.go
+++ ch5-performance/colStats/csv.go
@@ -27,20 +27,23 @@ func avg(data []float64) float64 {
 func csv2float(r io.Reader, column int) ([]float64, error) {
 	// Create the CSV Reader used to read in data from CSV files
 	cr := csv.NewReader(r)
+	cr.ReuseRecord = true
 
 	// Adjusting for 0 based index
 	column--
 
-	// Read in all CSV data
-	allData, err := cr.ReadAll()
-	if err != nil {
-		return nil, fmt.Errorf("cannot read data from file: %w", err)
-	}
-
 	var data []float64
 
 	// Looping through all records
-	for i, row := range allData {
+	for i := 0; ; i++ {
+		row, err := cr.Read()
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			return nil, fmt.Errorf("cannot read data from file: %w", err)
+		}
 		if i == 0 {
 			continue
 		}
--- ch5-performance/colStats/go.mod
+++ ch5-performance/colStats/go.mod
@@ -3,3 +3,5 @@ module github.com/nstoker/book-pp-pclag/performance/colStats
 go 1.17
 
 require github.com/pkg/errors v0.9.1
+
+require golang.org/x/tools v0.1.10 // indirect
